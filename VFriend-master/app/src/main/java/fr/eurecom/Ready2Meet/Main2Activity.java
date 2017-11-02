239e19d0a34048cc890ef7597d59ed979fee5950
kuechleralexander1@gmail.com <kuechleralexander1@gmail.com>
2017-11-02 21:16:16 +0100

    Changed ids for menu entries

--- VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/Main2Activity.java
+++ VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/Main2Activity.java
@@ -1,12 +1,8 @@
 package fr.eurecom.Ready2Meet;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,35 +13,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
-
-import static fr.eurecom.Ready2Meet.R.id.imageView;
-import static fr.eurecom.Ready2Meet.R.id.textView;
 
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -187,7 +165,7 @@ public class Main2Activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
         Fragment fragment;
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_add_event) {
 
             startActivity(new Intent(Main2Activity.this, AddEventActivity.class));
 
@@ -199,7 +177,7 @@ public class Main2Activity extends AppCompatActivity
             ft.replace(R.id.container_new, fragment);
             ft.commit();
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_messages) {
 
         } else if (id == R.id.nav_manage) {
 
