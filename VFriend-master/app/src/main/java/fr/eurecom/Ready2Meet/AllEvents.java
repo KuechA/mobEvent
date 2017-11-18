ba180ec1887fc2e6535ab5d9eb173e9613e0c31f
kuechleralexander1@gmail.com <kuechleralexander1@gmail.com>
2017-11-18 16:26:46 +0100

    First attemt at creating a useful start page

--- VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/AllEvents.java
+++ VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/AllEvents.java
@@ -99,8 +99,4 @@ public class AllEvents extends Fragment {
 
         return view;
     }
-
-
-
-
 }
--- VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/DashboardFragment.java
+++ VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/DashboardFragment.java
@@ -6,6 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eurecom.Ready2Meet.database.Event;
 
 
 /**
@@ -13,17 +24,41 @@ import android.view.ViewGroup;
  */
 public class DashboardFragment extends Fragment {
 
-
     public DashboardFragment() {
         // Required empty public constructor
     }
 
+    final List<Event> eventlist = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        // TODO: Retrieve list of events from firebase and add tem to the listView.
+
+        final ListView listView = (ListView) view.findViewById(R.id.start_page_list_of_events);
+        FirebaseDatabase.getInstance().getReference().child("Events")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        eventlist.clear();
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            Event eventread = snapshot.getValue(Event.class);
+                            eventlist.add(eventread);
+                        }
+                        ListViewAdapter_Event adapter = new ListViewAdapter_Event(getContext(), R.layout.row_events,eventlist);
+                        listView.setAdapter(adapter);
+                        adapter.notifyDataSetChanged();
+                        listView.invalidateViews();
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+        return view;
     }
 
 }
