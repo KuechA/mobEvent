3a701bbe1aff0f437e37618f73403bc292b8fb11
kuechleralexander1@gmail.com <kuechleralexander1@gmail.com>
2017-12-09 12:34:49 +0100

    OnClickListener for the items in the RecycleView showing all events

---
+++ VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/EventViewHolder.java
@@ -0,0 +1,45 @@
+package fr.eurecom.Ready2Meet;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import net.igenius.customcheckbox.CustomCheckBox;
+
+public class EventViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    TextView txtTitle, txtCategories, txtDescription, txtStarttime, txtEndtime, txtDate,
+            txtPlace, txtCurrent, txtCapacity;
+    ImageView eventpicture;
+    net.igenius.customcheckbox.CustomCheckBox participatingcheckbox;
+    LinearLayout participants;
+    View layout;
+
+    public EventViewHolder(View itemView) {
+        super(itemView);
+        layout = itemView;
+
+        txtTitle = (TextView) itemView.findViewById(R.id.txteventname);
+        txtCategories = (TextView) itemView.findViewById(R.id.txtcategories);
+        txtStarttime = (TextView) itemView.findViewById(R.id.txtstarttime);
+        txtEndtime = (TextView) itemView.findViewById(R.id.txtendtime);
+        txtDate = (TextView) itemView.findViewById(R.id.txtdate);
+        txtPlace = (TextView) itemView.findViewById(R.id.txtlocation);
+        txtCurrent = (TextView) itemView.findViewById(R.id.txtcurrent);
+        txtCapacity = (TextView) itemView.findViewById(R.id.txtcapacity);
+        txtDescription = (TextView) itemView.findViewById(R.id.txteventdescription);
+        eventpicture = (ImageView) itemView.findViewById(R.id.eventpicture);
+        participatingcheckbox = (CustomCheckBox) itemView.findViewById(R.id.participatingcheckbox);
+        participants = (LinearLayout) itemView.findViewById(R.id.participants);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(view.getContext(), "position " + getAdapterPosition() + " title: " +
+                txtTitle.getText(), Toast.LENGTH_LONG).show();
+    }
+}
