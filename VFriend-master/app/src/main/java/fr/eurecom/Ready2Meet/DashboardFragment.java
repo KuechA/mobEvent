ed2452e64fab0f36e9b1440dfdcc7eb42cd0f86a
kuechleralexander1@gmail.com <kuechleralexander1@gmail.com>
2017-12-18 09:09:33 +0100

    Slides++, show all events in dashboard

--- VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/DashboardFragment.java
+++ VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/DashboardFragment.java
@@ -4,10 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,28 +60,30 @@ public class DashboardFragment extends Fragment {
         });
 
         // TODO: Retrieve list of events from firebase and add tem to the listView.
-/*
-        final ListView listView = (ListView) view.findViewById(R.id.start_page_list_of_events);
+
+        final RecyclerView listView = (RecyclerView) view.findViewById(R.id
+                .start_page_list_of_events);
         FirebaseDatabase.getInstance().getReference().child("Events")
                 .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        eventlist.clear();
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            Event eventread = snapshot.getValue(Event.class);
-                            eventlist.add(eventread);
-                        }
-                        ListViewAdapter_Event adapter = new ListViewAdapter_Event(getContext(), R
-                        .layout.row_events,eventlist);
-                        listView.setAdapter(adapter);
-                        adapter.notifyDataSetChanged();
-                        listView.invalidateViews();
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
-                });
-                */
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                eventlist.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Event eventread = snapshot.getValue(Event.class);
+                    eventlist.add(eventread);
+                }
+                ListViewAdapter_Event adapter = new ListViewAdapter_Event(getContext(), eventlist);
+                listView.setAdapter(adapter);
+                listView.setLayoutManager(new LinearLayoutManager(DashboardFragment.this
+                        .getContext()));
+                adapter.notifyItemMoved(0, adapter.getItemCount() - 1);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
         return view;
     }
 
