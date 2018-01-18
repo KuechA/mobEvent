ee6e3e26bd04cee8093d9d6f1b466acbe42ce743
kuechleralexander1@gmail.com <kuechleralexander1@gmail.com>
2018-01-18 14:27:27 +0100

    Added code to service to start chat activity in intent

--- VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/ChatNotificationService.java
+++ VFriend-master/app/src/main/java/fr/eurecom/Ready2Meet/ChatNotificationService.java
@@ -1,7 +1,9 @@
 package fr.eurecom.Ready2Meet;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -14,23 +16,37 @@ public class ChatNotificationService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d("ChatNotificationService", "Message received from: " + remoteMessage.getFrom());
 
+        String eventId = null;
         // Check if message contains a data payload.
         if(remoteMessage.getData().size() > 0) {
             Log.d("ChatNotificationService", "Message data payload: " + remoteMessage.getData());
+            eventId = remoteMessage.getData().containsKey("EventId") ? remoteMessage.getData()
+                    .get("EventId") : null;
         }
 
         if(remoteMessage.getNotification() != null) {
             Log.d("ChatNotificationService", "Message Notification Body: " + remoteMessage
                     .getNotification().getBody());
             showNotification(remoteMessage.getNotification().getTitle(), remoteMessage
-                    .getNotification().getBody());
+                    .getNotification().getBody(), eventId);
         }
     }
 
-    private void showNotification(String title, String text) {
+    private void showNotification(String title, String text, String eventId) {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this
                 .getApplicationContext()).setSmallIcon(R.mipmap.ic_launcher).setContentTitle
                 (title).setContentText(text);
+
+        if(eventId != null) {
+            Intent resultIntent = new Intent(getApplicationContext(), ChatActivity.class);
+            resultIntent.putExtra("EventId", eventId);
+            resultIntent.putExtra("EventTitle", title);
+
+            PendingIntent resultPendingIntent = PendingIntent.getActivity(getApplicationContext()
+                    , 0, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            mBuilder.setContentIntent(resultPendingIntent);
+        }
 
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context
                 .NOTIFICATION_SERVICE);
